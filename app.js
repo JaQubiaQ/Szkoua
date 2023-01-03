a228abd01139ee12c5bc93d671797d20d2a3cdc0
Technikum <Technikum@ST102K14.cc.net>
2023-01-03 13:05:53 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function check(){
+    console.log("sdsd");
+    let num=parseInt(document.querySelector("#text").value);
+    if(checkint(num)){
+        if(num<0){
+            document.querySelector("#header").innerHTML="Wprowadzona liczba jest ujemna";
+        }
+        if(num>0){
+            document.querySelector("#header").innerHTML="Wprowadzona liczba jest dodatnia";
+        }
+    }else{
+        document.querySelector("#header").innerHTML="Wprowadzony tekst nie jest liczbą!";
+    }
+}
+
+function checkint(arg){
+    if(parseInt(arg)){
+        return 1;
+    }else{
+        return 0;
+    }
+}
